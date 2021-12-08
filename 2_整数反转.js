608479ed53cf8217c9a9eae7a3fa0fb4b6d1ab17
taylor <18379032623@163.com>
2021-12-08 23:51:09 +0800

    两数之合

---
+++ 2_整数反转.js
@@ -0,0 +1,26 @@
+/**
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+    如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+    假设环境不允许存储 64 位整数（有符号或无符号）。
+    示例 1：
+    输入：x = 123
+    输出：321
+    提示：
+    -231 <= x <= 231 - 1
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var reverse = function(x) {
+    if(x===0){
+        return 0;
+    }else if(x > 0){
+        var res = parseInt(x.toString().split("").reverse().join(""));
+    }else{
+        var res = parseInt(x.toString().split("").reverse().join(""));
+        res = -res;
+    }
+    // 判断越界
+    return res > (Math.pow(2,31)-1) || res < -Math.pow(2,31) ? 0 : res;
+};
